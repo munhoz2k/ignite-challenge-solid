656b0743b5361e2b46cb2ad84f60e9c1e5a8ef16
munhoz2k <lucasmunhozarruda@gmail.com>
2022-04-01 21:17:26 -0300

    Pega o Perfil do Usuário

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,14 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    try {
+      const userInfos = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.status(200).json(userInfos);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userInfos = this.usersRepository.findById(user_id);
+
+    if (!userInfos) {
+      throw new Error("User not found!");
+    }
+
+    return userInfos;
   }
 }
 
