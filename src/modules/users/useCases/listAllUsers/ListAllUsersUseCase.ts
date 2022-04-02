2ab96473b3b6911199ca5dc8fb98c2a0d8c22f4b
munhoz2k <lucasmunhozarruda@gmail.com>
2022-04-01 21:16:47 -0300

    Lista Todos Usuários

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userInfos = this.usersRepository.findById(user_id);
+
+    if (!userInfos) {
+      throw new Error("User not found!");
+    }
+    if (!userInfos.admin) {
+      throw new Error("Can't list users for a non admin user!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
